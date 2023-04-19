ed4838c1b7980fe4a2c5b7aec9b5d6291c4d4e9f
Brunet <brunet.thibaut07@gmail.com>
2023-04-19 09:21:05 +0200

    feat(requests): end admin requests config

--- assets/JS/Frontend/displayForm.js
+++ assets/JS/Frontend/displayForm.js
@@ -5,10 +5,14 @@ if (divForm && btn) {
     btn.addEventListener('click', () => {
         if (getComputedStyle(divForm).display == 'none') {
             divForm.style.display = 'block';
+            let posDivForm = divForm.getBoundingClientRect();
             btn.innerHTML = 'Enlever le nouveau message';
+            window.scrollTo(posDivForm.x, posDivForm.y);
         } else {
+            let postBtn = btn.getBoundingClientRect();
             divForm.style.display = 'none';
             btn.innerHTML = 'Ajouter un nouveau message';
+            window.scrollTo(postBtn.x + 1, postBtn.y + 1);
         }
     })
 }
--- src/Controller/Backend/RequestController.php
+++ src/Controller/Backend/RequestController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller\Backend;
 
+use App\Entity\Request;
+use App\Entity\Messages;
+use App\Form\MessageType;
 use App\Repository\RequestRepository;
 use App\Repository\MessagesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request as req;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/requests')]
@@ -17,11 +22,43 @@ class RequestController extends AbstractController
     ) {
     }
 
-    #[Route('', name: 'app.admin.requests', methods: ['GET', 'POST'])]
+    #[Route('', name: 'app.admin.requests', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('Backend/Requests/index.html.twig', [
             'requests' => $this->requestRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/show/{id}', name: 'app.admin.requests.show', methods: ['GET', 'POST'])]
+    public function show(Request $request, req $req): Response|RedirectResponse
+    {
+        $message = new Messages;
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message->setAuthor($this->getUser());
+            $message->setRequest($request);
+            if ($req->attributes->get('_route') == 'app.admin.requests.show') {
+                $message->setSupport(true);
+            } else {
+                $message->setSupport(false);
+            }
+
+            $this->messagesRepository->save($message, true);
+
+            $this->addFlash('success', 'Message ajouté avec succès');
+
+            return $this->redirectToRoute('app.admin.requests.show', [
+                'id' => $request->getId(),
+            ]);
+        }
+
+        return $this->render('Backend/Requests/show.html.twig', [
+            'request' => $request,
+            'messages' => $request->getMessages(),
+            'form' => $form->createView(),
         ]);
     }
 }
